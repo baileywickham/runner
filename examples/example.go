4fb371e4eb429ecf2fda1a8770af87a229faec25
baileywickham <baileywickham@gmail.com>
2020-02-12 15:19:19 -0800

    update formatting

--- examples/example.go
+++ examples/example.go
@@ -4,8 +4,15 @@ import r "github.com/baileywickham/runner"
 
 func main() {
 	shell := r.NewShell()
-	c1 := r.Command{"echo", echo, "echo: print a string to stdout"}
-	c2 := r.Command{"addTen", addTen, "addTen: takes and int and adds 10"}
+	c1 := r.Command{
+		Cmd:      "echo",
+		Callback: echo,
+		Helptext: "echo: print a string to stdout"}
+
+	c2 := r.Command{
+		Cmd:      "addTen",
+		Callback: addTen,
+		Helptext: "addTen: takes and int and adds 10"}
 	shell.Add_command(c1, c2) // Add command uses variadic arguments
 	shell.Start()
 
